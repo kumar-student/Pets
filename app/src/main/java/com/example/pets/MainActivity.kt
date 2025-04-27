1841a854b750664c80b4bbe13c255ffde98b071d
kumar <kumar.student100564@gmail.com>
2025-04-27 12:58:42 -0700

    Layouts

--- app/src/main/java/com/example/pets/MainActivity.kt
+++ app/src/main/java/com/example/pets/MainActivity.kt
@@ -4,23 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.tooling.preview.Preview
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,15 +19,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
-    var count by remember { mutableStateOf(0) }
-
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        Text(text = "Counter: $count", style = MaterialTheme.typography.bodyLarge)
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { count++ }) {
-            Text("Increment")
-        }
-    }
+    PacktColumn()
+    // PacktRow()
+    // PacktBox()
+    // PacktLazyColumn()
+    // PacktLazyRow()
+    // PacktLazyVerticalGrid()
+    // PacktLazyHorizontalGrid()
+    // PacktConstraintLayout()
 }
