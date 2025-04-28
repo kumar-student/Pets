94c3ab74678ce7a9229dbd42cb81744f38bd2187
kumar <kumar.student100564@gmail.com>
2025-04-27 22:32:45 -0700

    Window size dependency

--- app/src/main/java/com/example/pets/MainActivity.kt
+++ app/src/main/java/com/example/pets/MainActivity.kt
@@ -19,6 +19,7 @@ import com.example.pets.ui.theme.PetsTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val deviceFoldingPostureFlow = WindowInfoTracker
         enableEdgeToEdge()
         setContent {
             PetsTheme {
